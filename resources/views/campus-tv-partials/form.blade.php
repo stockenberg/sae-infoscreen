6410bb22868779c0302ebd305a80ac8e170ac8de
Marten Stockenberg <MStockenberg@googlemail.com>
2017-11-28 14:28:32 +0100

    updated event syntax

--- resources/views/campus-tv-partials/form.blade.php
+++ resources/views/campus-tv-partials/form.blade.php
@@ -7,33 +7,33 @@
 
         <div class="form-group">
             <label for="">Erste Überschrift</label>
-            <input type="text" class="form-control" name="headline" id="" value="{{$event->headline ?? null}}" placeholder="">
+            <input type="text" class="form-control" name="headline" id="" value="{{($event) ? $event->headline : ''}}" placeholder="">
         </div>
 
         <div class="form-group">
             <label for="">Details</label>
-            <input type="text" class="form-control" name="details" id="" value="{{$event->details ?? null}}" placeholder="">
+            <input type="text" class="form-control" name="details" id="" value="{{($event) ? $event->details : ''}}" placeholder="">
         </div>
 
         <div class="form-group">
             <label for="">zweite Überschrift</label>
-            <input type="text" class="form-control" name="subheadline" id="" value="{{$event->subheadline ?? null}}" placeholder="">
+            <input type="text" class="form-control" name="subheadline" id="" value="{{($event) ? $event->subheadline: ''}}" placeholder="">
         </div>
 
        <div class="form-group">
            <label for="">Hintergrund</label>
            <select name="background" id="inputID" class="form-control">
-               <option value="bgBlue" {{$event->background ?? null === "bgBlue" ? 'selected' : ''}}>Blau</option>
-               <option value="bgLightblue" {{$event->background ?? null === "bgLightblue" ? 'selected' : ''}}>Hellblau</option>
-               <option value="bgYellow" {{$event->background ?? null === "bgYellow" ? 'selected' : ''}}>Gelb</option>
-               <option value="bgOrange" {{$event->background ?? null === "bgOrange" ? 'selected' : ''}}>Orange</option>
+               <option value="bgBlue" {{$event && $event->background === "bgBlue" ? 'selected' : ''}}>Blau</option>
+               <option value="bgLightblue" {{$event && $event->background === "bgLightblue" ? 'selected' : ''}}>Hellblau</option>
+               <option value="bgYellow" {{$event && $event->background === "bgYellow" ? 'selected' : ''}}>Gelb</option>
+               <option value="bgOrange" {{$even && $event->background === "bgOrange" ? 'selected' : ''}}>Orange</option>
            </select>
        </div>
 
         <div class="form-group">
             <div class="checkbox">
                 <label>
-                    <input type="checkbox" {{$event->active ?? null == 1 ? 'checked' : ''}} name="active" value="1" id="">
+                    <input type="checkbox" {{$event && $event->active == 1 ? 'checked' : ''}} name="active" value="1" id="">
                     Veröffentlichen
                 </label>
             </div>
