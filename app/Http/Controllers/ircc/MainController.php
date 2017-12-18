1bd55f3e78465ef82cdbbee6beb7dd34f0c1193c
Marten Stockenberg <MStockenberg@googlemail.com>
2017-12-18 17:07:49 +0100

    order by rating

--- app/Http/Controllers/ircc/MainController.php
+++ app/Http/Controllers/ircc/MainController.php
@@ -14,33 +14,25 @@ class MainController extends Controller
 {
 
 	private $id;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    	$departments = Department::all();
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index()
-	{
-		$departments = Department::all();
-
-		$contacts = IndustrialContact::with([
-			'departments' => function ($query) {
-				$query->orderBy('likes', 'DESC');
-			},
-			'histories'   => function ($query) {
-				$query->orderBy('created_at', 'DESC');
-			},
-			'jobs'        => function ($query) {
-				$query->orderBy('created_at', 'DESC');
-			},
-			'students',
-		])
+    	$contacts = IndustrialContact::with([
+    		'departments',
+			'histories' => function ( $query ){ $query->orderBy('created_at', 'DESC'); },
+			'jobs' => function ( $query ){ $query->orderBy('created_at', 'DESC'); },
+			'students'])
+			->orderBy('likes', 'DESC')
 			->get();
 
-		return view('ircc.main',
-			['contacts' => $contacts, 'departments' => $departments]);
-	}
+        return view('ircc.main', ['contacts' => $contacts, 'departments' => $departments]);
+    }
 
 	public function updateContact(Request $request)
 	{
@@ -68,7 +60,7 @@ class MainController extends Controller
 
 		return $this->findIndustrialContactById($request->input('active'));
 
-	}
+    }
 
 	public function addJob(Request $request)
 	{
@@ -98,18 +90,13 @@ class MainController extends Controller
 	{
 		$contacts = IndustrialContact::with([
 			'departments',
-			'histories' => function ($query) {
-				$query->orderBy('created_at', 'DESC');
-			},
-			'jobs'      => function ($query) {
-				$query->orderBy('created_at', 'DESC');
-			},
-			'students',
-		])
+			'histories' => function ( $query ){ $query->orderBy('created_at', 'DESC'); },
+			'jobs' => function ( $query ){ $query->orderBy('created_at', 'DESC'); },
+			'students'])
 			->find($id);
 
 		return $contacts;
-	}
+    }
 
 	public function indexDepartments($id)
 	{
@@ -117,113 +104,103 @@ class MainController extends Controller
 
 		$contacts = IndustrialContact::with([
 			'departments',
-			'histories' => function ($query) {
-				$query->orderBy('created_at', 'DESC');
-			},
-			'jobs'      => function ($query) {
-				$query->orderBy('created_at', 'DESC');
-			},
-			'students',
-		])
+			'histories' => function ( $query ){ $query->orderBy('created_at', 'DESC'); },
+			'jobs' => function ( $query ){ $query->orderBy('created_at', 'DESC'); },
+			'students'])
 			->whereHas('departments', function ($q) {
 				$q->where('department_id', $this->id);
 			})
 			->get();
 
 		return $contacts;
-	}
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create()
-	{
-		$departments = Department::all();
-		return view('ircc.create', ['departments' => $departments]);
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    	$departments = Department::all();
+        return view('ircc.create', ['departments' => $departments]);
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request)
-	{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
 
-		$this->validate($request, [
-			'name'                => 'required',
-			'likes'               => 'required',
-			'primary_contact'     => 'required',
-			'last_contact'        => 'required',
+    	$this->validate($request, [
+    		'name' => 'required',
+			'likes' => 'required',
+			'primary_contact' => 'required',
+			'last_contact' => 'required',
 			'last_contact_person' => 'required',
 
 		]);
 
-		$industrialContact = new IndustrialContact();
+        $industrialContact = new IndustrialContact();
 
-		$industrialContact->name = $request->name;
-		$industrialContact->likes = $request->likes;
-		$industrialContact->primary_contact = $request->primary_contact;
-		$industrialContact->last_contact = $request->last_contact;
-		$industrialContact->last_contact_person = $request->last_contact_person;
-		$industrialContact->adress = $request->adress;
+        $industrialContact->name = $request->name;
+        $industrialContact->likes = $request->likes;
+        $industrialContact->primary_contact = $request->primary_contact;
+        $industrialContact->last_contact = $request->last_contact;
+        $industrialContact->last_contact_person = $request->last_contact_person;
+        $industrialContact->adress = $request->adress;
 
-		$industrialContact->save();
-		$industrialContact->departments()->attach($request->departments);
+        $industrialContact->save();
+        $industrialContact->departments()->attach($request->departments);
 
 		return redirect()->back();
 	}
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int $id
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int $id
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  int                      $id
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, $id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int $id
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
